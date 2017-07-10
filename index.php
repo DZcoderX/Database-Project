e9ecaacee35c2bc68fccbeda4abc15ea6c318c1d
David Zhang <ruiqing.zhang18@gmail.com>
2017-07-10 01:08:51 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+  <title>Food Chain Manager</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    
+<!--    <link href="styles1.css" rel="stylesheet" type="text/css">-->
+    
+    
+    
+</head>
+<body>
+    
+    
+  <nav class="red" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#" class="brand-logo">A Database Project</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Help</a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="#">Help</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h1 class="header center text-lighten-2">Welcome to the Food-Chain Management System!</h1>
+        
+          
+        <div class="row center">
+          <a class="btn-large waves-effect waves-light orange">Get Started</a>
+        </div>
+        <br><br>
+
+      </div>
+    </div>
+    <div class="parallax"><img src="background1.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+
+<!--============================================== -->
+    <footer class="page-footer red">
+  <div class="container">
+    <div class="section">        
+        <br>
+      <!--   Icon Section   -->
+      <div class="row">
+          
+        <div class="space"></div>
+          <div class="col s12 m6 l3">
+              
+                <a class="waves-effect waves-light btn-large orange" href="add.php"><i class="material-icons right">add</i>
+                    Add
+                  </a>
+         </div>
+          <div class="col s12 m6 l3">
+              
+                <a class="waves-effect waves-light btn-large orange" href="edit.php"><i class="material-icons right">edit</i>Edit</a>
+                 
+         </div>
+         <div class="col s12 m6 l3">
+                 
+                <a class="waves-effect waves-light btn-large orange" href="delete.php"><i class="material-icons right">delete</i>Delete</a>
+         </div>
+         <div class="col s12 m6 l3">
+                 
+                  <a class="waves-effect waves-light btn-large orange" href="view.php"><i class="material-icons right">visibility</i>Look</a>
+          </div>
+          </div>
+          <div class="space"></div>
+      
+    </div>
+  </div>
+        
+    </footer>
+<!--==============================================-->
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+      <div class="container">
+
+      </div>
+    </div>
+    <div class="parallax"><img src="background2.jpg" alt="Unsplashed background img 2"></div>
+  </div>
+
+    
+<!--    3nd content area-->
+    <footer class="page-footer red">
+  <div class="container">
+    <div class="section" >
+
+      <div class="row">
+        <div class="col s12 center">
+          <h3><i class="mdi-content-send brown-text"></i></h3>
+          <h4 class="white-text">We are here to help.</h4>
+          <p class="white-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+      </footer>
+
+
+  <div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+
+    </div>
+    <div class="parallax"><img src="background3.jpg" alt="Unsplashed background img 3"></div>
+  </div>
+
+    
+  <footer class="page-footer red">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Settings</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+          
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by Jon, Danial, and David
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
